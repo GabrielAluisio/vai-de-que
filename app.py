1e75eeb70254fe1829b0a8778942368ae2ee38e0
GabrielDiniz <aluisiogabrieldiniz@gmail.com>
2025-07-16 20:58:26 -0300

    protótipo inicial com conexão à API OpenWeather e exibição do clima básico

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from ultils.clima import obter_previsão
+from ultils.clima import obter_previsao
 
 if __name__ == '__main__':
 
     city_name = input("Digite sua cidade: ")
-    obter_previsão(city_name)
+    obter_previsao(city_name)
+    
--- ultils/clima.py
+++ ultils/clima.py
@@ -6,15 +6,32 @@ def acessar_api(api_key, city):
 
 
     url = "https://api.openweathermap.org/data/2.5/weather?"
-    url_completa = f"{url}&appid={api_key}&q={city}"
+    url_completa = f"{url}appid={api_key}&q={city}&units=metric"
     
-    reponse = requests.get(url_completa)
+    response = requests.get(url_completa)
 
-    return reponse.json()
+    return response.json()
 
-def obter_previsão(city_name):
+def obter_previsao(city_name):
 
     api_key = "abe4201233b3a20dca48e1a3498d45d3"
     dados = acessar_api(api_key, city_name)
 
-    print(dados)
+    if dados['cod'] == 401:
+        print('Problema durante a requisição\n'
+            f"Mensagem: {dados['message']}")
+        
+    elif dados['cod'] != 404:
+        clima = dados['weather'][0]['main']
+        temperatura = dados['main']['temp']
+        sensacao = dados['main']['feels_like']
+        vento = dados['wind']['speed']
+        
+        print(f'O clima em {city_name}: {clima}')
+        print(f'Temperatura: {temperatura}°C')
+        print(f'Sensação: {sensacao}°C')
+        print(f'Velocidade do vento: {vento}m/s')
+
+    else:
+        print('Cidade não encontrada!')
+    
