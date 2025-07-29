4b870cee2069f583b90c27e92adcbf8e8f2249f4
GabrielDiniz <aluisiogabrieldiniz@gmail.com>
2025-07-29 13:54:07 -0300

    Voltei para a estrutura original do projeto. Após reiniciar os containers e o NGROK, tudo voltou a funcionar corretamente. Não sei exatamente o que causou o erro antes, mas agora o bot está respondendo como esperado.

--- whatsapp/bot.py
+++ whatsapp/bot.py
@@ -1,34 +1,10 @@
 import requests
 from utils.clima import definir_clima  # sua função que gera texto clima
 
-def enviar_mensagem_whatsapp(texto):
-    url = "http://127.0.0.1:8080/message/sendText/vou-de-que"  # URL da API do WhatsApp (local ou externa)
-    headers = {
-        "Content-Type": "application/json",
-        "apikey": "16645164840"  # sua chave da API
-    }
-    payload = {
-        "number": "5511932222897",
-        "options": {
-            "delay": 1000,
-            "presence": "composing"
-        },
-        "textMessage": {
-            "text": texto
-        }
-    }
-    response = requests.post(url, json=payload, headers=headers)
-    print("Status:", response.status_code)
-    print("Resposta:", response.text)
-
-
-import requests
-from utils.clima import definir_clima  # sua função que gera texto clima
-
 cidade_atual = 'sao paulo'  # padrão inicial
 
 def enviar_mensagem_whatsapp(texto):
-    url = "http://127.0.0.1:8080/message/sendText/vai-de-que"  # sua URL API WhatsApp
+    url = "http://127.0.0.1:8080/message/sendText/vou-de-que"  # sua URL API WhatsApp
     headers = {
         "Content-Type": "application/json",
         "apikey": "16645164840"  # sua chave da API
